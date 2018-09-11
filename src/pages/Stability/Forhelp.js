cd912d0066512db43226483cbf67bb3730a0684d
tianpengfei <tpf1220@sina.com>
2018-09-11 17:21:59 +0800

    new  pages

--- src/pages/Stability/Forhelp.js
+++ src/pages/Stability/Forhelp.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import {
+  Card
+} from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
-const Forhelp = () => {
-  return (
-    <div>
-      求助数据
-    </div>
-  )
+@connect()
+class Forhelp extends PureComponent {
+  
+  render() {
+    return (
+      <PageHeaderWrapper title="求助数据" >
+        <Card bordered={false}>
+            这里 是求助数据页面           
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
 }
 
-export default Forhelp
+export default Forhelp;
--- src/pages/Stability/ProblemClass.js
+++ src/pages/Stability/ProblemClass.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import {
+  Card
+} from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
-const ProblemClass = () => {
-  return (
-    <div>
-      问题课堂数据
-    </div>
-  )
+@connect()
+class ProblemClass extends PureComponent {
+  
+  render() {
+    return (
+      <PageHeaderWrapper title="问题课堂数据" >
+        <Card bordered={false}>
+            这里是问题课堂数据页面           
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
 }
 
-export default ProblemClass
+export default ProblemClass;

57bb3b34fa5c09d311e398d745612ee69fcde2f9
lzg <1649973704@qq.com>
2018-10-11 17:24:54 +0800

    LoopDetail

--- src/components/Loop/DetailSelect.js
+++ src/components/Loop/DetailSelect.js
@@ -1,5 +1,5 @@
 import React, { PureComponent, Fragment } from 'react'
-import { Checkbox, Radio, DatePicker, Form, Select, Button} from 'antd'
+import { Checkbox, Radio, DatePicker, Form, Select, Button, Input} from 'antd'
 import moment from 'moment'
 
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker
@@ -12,34 +12,53 @@ export class DetailSelect extends PureComponent {
       <Fragment>
           <DatePicker style={{marginLeft:"30px"}} />
           <div>
-            <Select defaultValue="allTextBook" style={{ width: 120,marginLeft:30 }} >
+            <Select defaultValue="allTextBook" style={{ width: 130,marginLeft:30 }} >
               <Option value="allTextBook">全部课程</Option>
               <Option value="reach">Reach</Option>
               <Option value="HE">HE</Option>
             </Select>
-            <Select defaultValue="allType" style={{ width: 120,marginLeft:30 }} >
+            <Select defaultValue="allType" style={{ width: 130,marginLeft:30 }} >
               <Option value="allType">全部类型</Option>
               <Option value="formal">正式课</Option>
               <Option value="experience">体验课</Option>
             </Select>
-            <Select defaultValue="allLevel" style={{ width: 120,marginLeft:30 }} >
+            <Select defaultValue="allLevel" style={{ width: 130,marginLeft:30 }} >
               <Option value="allLevel">全部级别</Option>
               <Option value="A">G1A</Option>
               <Option value="B">G2A</Option>
             </Select>
-            <Select defaultValue="note" style={{ width: 120,marginLeft:30 }} >
+            <Select defaultValue="note" style={{ width: 130,marginLeft:30 }} >
               <Option value="note">讲义情况</Option>
               <Option value="1">是</Option>
               <Option value="2">否</Option>
             </Select>
-            <Select defaultValue="proview" style={{ width: 120,marginLeft:30 }} >
+            <Select defaultValue="proview" style={{ width: 130,marginLeft:30 }} >
               <Option value="proview">预习情况</Option>
               <Option value="1">是</Option>
               <Option value="2">否</Option>
             </Select>
           </div>
-          <Button type="primary" style={{ marginLeft:30 }}>查询</Button>
-          <Button type="primary" style={{ marginLeft:30 }}>查询</Button>
+          <br />
+          <div>
+            <Select defaultValue="homework" style={{ width: 130,marginLeft:30 }} >
+              <Option value="homework">作业情况</Option>
+              <Option value="1">是</Option>
+              <Option value="2">否</Option>
+            </Select>
+            <Select defaultValue="port" style={{ width: 130,marginLeft:30 }} >
+              <Option value="port">报告情况</Option>
+              <Option value="1">是</Option>
+              <Option value="2">否</Option>
+            </Select>
+            <Input placeholder="学员姓名/学员手机号" style={{ width: 130,marginLeft:30 }}/>
+            <Select defaultValue="classTeacher" style={{ width: 130,marginLeft:30 }} >
+              <Option value="classTeacher">所属班主任</Option>
+              <Option value="1">AA</Option>
+              <Option value="2">BB</Option>
+            </Select>
+            <Button type="primary" style={{ marginLeft:30 }}>查询</Button>
+          </div>
+          
       </Fragment>
     )
   }
--- src/pages/Loop/LoopDetail.js
+++ src/pages/Loop/LoopDetail.js
@@ -5,10 +5,32 @@ import { message, Row, Col, Card, Spin } from 'antd';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 import DetailSelect from '@/components/Loop/DetailSelect';
+import TableData from '@/components/Loop/TableData';
 @connect()
 class LoopDetail extends Component {
   
   render() {
+    const columns = [
+      {title:'课堂ID', dataIndex:'id',key:'id'}, 
+      {title:'上课时间', dataIndex:'classTime', key:'classTime'},
+      {title:'学生姓名/手机号', dataIndex:'nameAndTel', key:'nameAndTel'},
+      {title:'讲义', dataIndex:'note', key:'note'},
+      {title:'预习', dataIndex:'proview', key:'proview'},
+      {title:'作业', dataIndex:'homework', key:'homework'},
+      {title:'报告',dataIndex:'port', key:'port'}
+    ]
+    const data = [
+      {
+        key:0,
+        id:123123,
+        classTime:'2018.9.8-09:10',
+        nameAndTel:'张伟/15269391727',
+        note:'是',
+        proview:'是',
+        homework:'是',
+        port:'否',
+      }
+    ]
     return (
       <PageHeaderWrapper title="语文课学习详情">
         <Spin spinning={false} size="large">
@@ -17,6 +39,7 @@ class LoopDetail extends Component {
           </Card>
           <br />
           <Card bordered={false}>
+            <TableData colType={columns} dataArr={data} />
           </Card>
         </Spin>
       </PageHeaderWrapper>

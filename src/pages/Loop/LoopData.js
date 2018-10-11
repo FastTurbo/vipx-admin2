640674a161ccf2447b6cd11500169dcf8d3c2fef
tianpengfei <tpf1220@sina.com>
2018-10-11 10:43:07 +0800

    add frame

--- src/pages/Loop/LoopData.js
+++ src/pages/Loop/LoopData.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import moment from 'moment'
+import { message, Row, Col, Card, Spin } from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+@connect()
+class LoopData extends Component {
+  
 
-const LoopData = () => {
-  return (
-    <div>
-      闭环 数据
-    </div>
-  )
+  
+  render() {
+    
+    return (
+      <PageHeaderWrapper title="语文课闭环数据">
+        <Spin spinning={false} size="large">
+          <Card bordered={false}>
+          </Card>
+          <br/>
+          <Card bordered={false}>
+          </Card>
+          <br />
+
+          <Card bordered={false}>
+          </Card>
+        </Spin>
+      </PageHeaderWrapper>
+    );
+  }
 }
 
-export default LoopData
+export default LoopData;

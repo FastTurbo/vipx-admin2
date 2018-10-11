a407147bea45c6e5a4d0badfe8b12418d9386514
tianpengfei <tpf1220@sina.com>
2018-10-11 10:44:35 +0800

    add frame

--- src/pages/Loop/LoopDetail.js
+++ src/pages/Loop/LoopDetail.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
-const LoopDetail = () => {
-  return (
-    <div>
-      闭环学习详情
-    </div>
-  )
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import moment from 'moment'
+import { message, Row, Col, Card, Spin } from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+@connect()
+class LoopDetail extends Component {
+  
+  render() {
+    return (
+      <PageHeaderWrapper title="语文课学习详情">
+        <Spin spinning={false} size="large">
+          <Card bordered={false}>
+          </Card>
+          <br />
+          <Card bordered={false}>
+          </Card>
+        </Spin>
+      </PageHeaderWrapper>
+    );
+  }
 }
-
-export default LoopDetail
+export default LoopDetail;

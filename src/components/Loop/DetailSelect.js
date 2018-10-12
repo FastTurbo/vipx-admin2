b9e8f48bf0714314c499d8fe2937f18cd280d737
lzg <1649973704@qq.com>
2018-10-12 16:13:28 +0800

    updata

--- src/components/Loop/DetailSelect.js
+++ src/components/Loop/DetailSelect.js
@@ -10,64 +10,48 @@ export class DetailSelect extends PureComponent {
   render() {
     return (
       <Fragment>
-        <Form layout="inline">
-          <FormItem>
-            <DatePicker />
-          </FormItem>
-          <FormItem>
-            <Select defaultValue="allTextBook" style={{width:160}}>
+          <DatePicker style={{marginLeft:"30px"}} />
+          <div>
+            <Select defaultValue="allTextBook" style={{ width: 130,marginLeft:30 }} >
               <Option value="allTextBook">全部课程</Option>
               <Option value="reach">Reach</Option>
               <Option value="HE">HE</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="allType" style={{width:160}}>
+            <Select defaultValue="allType" style={{ width: 130,marginLeft:30 }} >
               <Option value="allType">全部类型</Option>
               <Option value="formal">正式课</Option>
               <Option value="experience">体验课</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="allLevel" style={{width:160}}>
+            <Select defaultValue="allLevel" style={{ width: 130,marginLeft:30 }} >
               <Option value="allLevel">全部级别</Option>
               <Option value="A">G1A</Option>
               <Option value="B">G2A</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="note" style={{width:160}}>
+            <Select defaultValue="note" style={{ width: 130,marginLeft:30 }} >
               <Option value="note">讲义情况</Option>
               <Option value="1">是</Option>
               <Option value="2">否</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="proview" style={{width:160}}>
+            <Select defaultValue="proview" style={{ width: 130,marginLeft:30 }} >
               <Option value="proview">预习情况</Option>
               <Option value="1">是</Option>
               <Option value="2">否</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="homework" style={{width:160}}>
+          </div>
+          <br />
+          <div>
+            <Select defaultValue="homework" style={{ width: 130,marginLeft:30 }} >
               <Option value="homework">作业情况</Option>
               <Option value="1">是</Option>
               <Option value="2">否</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="port" style={{width:160}}>
+            <Select defaultValue="port" style={{ width: 130,marginLeft:30 }} >
               <Option value="port">报告情况</Option>
               <Option value="1">是</Option>
               <Option value="2">否</Option>
             </Select>
-          </FormItem>
-           <FormItem>
-            <Input placeholder="学员姓名/学员手机号"/>
-          </FormItem>
-           <FormItem>
-            <Select defaultValue="classTeacher" style={{width:160}}>
+            <Input placeholder="学员姓名/学员手机号" style={{ width: 130,marginLeft:30 }}/>
+            <Select defaultValue="classTeacher" style={{ width: 130,marginLeft:30 }} >
               <Option value="classTeacher">所属班主任</Option>
               <Option value="1">AA</Option>
               <Option value="2">BB</Option>
